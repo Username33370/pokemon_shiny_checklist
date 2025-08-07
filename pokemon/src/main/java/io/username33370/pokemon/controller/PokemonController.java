700a810781a1c4a1835aa75b3bd13ed51337ef0e
Username33370 <wiggles@naver.com>
2025-08-07 16:45:14 +0900

    Initial commit

---
+++ pokemon/src/main/java/io/username33370/pokemon/controller/PokemonController.java
@@ -0,0 +1,26 @@
+package io.username33370.pokemon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/pokemon")
+public class PokemonController {
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Integer> numbers = IntStream.rangeClosed(1,1025).boxed().collect(Collectors.toList());
+        model.addAttribute("numbers",numbers);
+        return "list";
+    }
+
+    @GetMapping("/hunting")
+    public String hunting() {
+        return "hunting";
+    }
+}
---
+++ pokemon/src/main/java/io/username33370/pokemon/service/PokemonService.java
@@ -0,0 +1,7 @@
+package io.username33370.pokemon.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PokemonService {
+}
